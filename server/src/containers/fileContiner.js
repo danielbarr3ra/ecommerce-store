10b1df7092d09378adb74e9d59cbb757869ed6af
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-10-02 18:12:48 -0500

    structure of data container

---
+++ server/src/containers/fileContiner.js
@@ -0,0 +1,25 @@
+class fileContainer {
+    constructor(path) {
+        this.path = path;
+    }
+
+    getAll() {
+        return []
+    }
+    get(id) {
+        return []
+    }
+    update(id) {
+        return []
+    }
+    save(obj) {
+        return []
+    }
+    delete(id) {
+        return []
+    }
+    deleteAll() {
+        return []
+    }
+
+}
