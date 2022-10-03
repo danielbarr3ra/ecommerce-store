2fd981a2b6ce6cdd2d8d5ff26e8247f9b5a4ec97
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-10-02 20:20:54 -0500

    adding fileContainer method

--- server/src/containers/fileContiner.js
+++ server/src/containers/fileContiner.js
@@ -1,25 +1,119 @@
+import fs from 'fs/promises'
+// make sure you use the promises instead of the fs will need to parse and unparse use helper functions for that
+
+
 class fileContainer {
+    /**
+     * SCHEMA the database will sotre:
+     * title, price, thumbnail, id for Producst
+     * 
+     * For cars we can do later on
+     */
     constructor(path) {
         this.path = path;
     }
 
-    getAll() {
-        return []
+    /**
+     * 
+     * TODO once I have ocnfidecne on the methods you can remove the try catch
+     */
+
+
+    async getAll() {
+        try {
+            const allItems = await fs.readFile(this.path)
+            return this.convertToJson(allItems)
+        } catch (error) {
+            console.log('there was en error fetching the items');
+            return error;
+        }
     }
-    get(id) {
-        return []
+
+
+    async get(id) {
+        try {
+            const allItems = await this.getAll()
+            const item = allItems.filter((item) => {
+                return item.id == id;
+            })
+            if (!item) {
+                throw error('There was no item that matched the id')
+            }
+            return item
+        } catch (error) {
+            console.log('there was en error fetching the items');
+            return error;
+        }
     }
-    update(id) {
-        return []
+    async update(id, obj) {
+        try {
+            let allItems = await this.getAll();
+            const index = allItems.findIndex((obj) => {
+                return obj.id == id
+            })
+            const updated = { ...obj, id: id }
+            if (!index) {
+                throw error('there is no item that matched that id')
+            }
+            allItems[index] = updated;
+
+            await this.saveAll(allItems)
+
+        } catch (error) {
+            console.log('there was an error updating the item')
+            return error;
+        }
     }
-    save(obj) {
-        return []
+    async save(obj) {
+        try {
+            let allItems = await this.getAll();
+            let lastId = allItems[allItems.length - 1].id;
+            allItems.push({ ...obj, id: ++lastId })
+            await this.saveAll(allItems)
+        } catch (error) {
+            console.log('there was an error saving one of the objects')
+            return error
+        }
     }
-    delete(id) {
-        return []
+
+    async saveAll(anArray) {
+        try {
+            await fs.writeFile(anArray)
+        } catch (error) {
+            console.log('there was an error saving all the objects')
+            return error
+        }
     }
-    deleteAll() {
-        return []
+    async delete(id) {
+        try {
+            let allItems = await this.getAll()
+            allItems.filter((obj) => {
+                return obj.id != id;
+            })
+            await this.saveAll(allItems);
+        } catch (error) {
+            console.log('there was an error deleting the item');
+            return error;
+        }
+    }
+    async deleteAll() {
+        try {
+            await this.saveAll([]);
+        } catch (error) {
+            console.log('there was an error deleting all the values')
+            return error;
+        }
+    }
+
+
+    // just for readibility (for me at least)
+    convertToJson(aString) {
+        return JSON.parse(aString)
+    }
+    convertToString(json) {
+        return JSON.stringify(json)
     }
 
 }
+
+export default fileContainer
