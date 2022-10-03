799cf7d7cc0936e72c3c7a018a6a398bb1999bea
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-10-02 23:04:09 -0500

    creating api

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,9 @@
 import express from 'express'
+import {
+    productsDao as ProductsApi,
+    cartsDao as CartsApi
+} from './daos/index.js'
+
 const { Router } = express
 
 // later on should import the daos
@@ -31,10 +36,41 @@ const onlyAdmin = (req, res, next) => {
 const productsRouter = new Router()
 
 productsRouter.get('/', async (req, res) => {
+    console.log('getting all the elements')
+    const allProducts = await ProductsApi.getAll();
+    res.json(allProducts)
+})
+
+
+productsRouter.get('/:id', async (req, res) => {
+    console.log('testing the product router')
+    const item = ProductsApi.get()
+    res.json(item)
+})
+
+
+productsRouter.post('/', onlyAdmin, async (req, res) => {
     console.log('testing the product router')
     res.json([])
 })
 
+
+productsRouter.put('/:id', onlyAdmin, async (req, res) => {
+    console.log('testing the product router')
+    res.json([])
+})
+
+
+productsRouter.delete('/:id', onlyAdmin, async (req, res) => {
+    console.log('testing the product router')
+    res.json([])
+})
+
+
+/**
+ * TODO add the cart ones and the product within cart ones
+ * 
+ */
 const shoppingCartsRouter = new Router()
 shoppingCartsRouter.get('/', async (req, res) => {
     console.log('testing the product router')
