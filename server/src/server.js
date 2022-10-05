177a8e33fc6f8431676de32c7fde4ad977223b6d
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-10-04 23:16:20 -0500

    server.js comments

--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,6 @@ import express from 'express'
 import { productsRouter } from './routes/products.js'
 import { shoppingCartsRouter } from './routes/carts.js'
 
-const { Router } = express
-
-// later on should import the daos
 
 const app = express()
 
@@ -12,6 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
 app.use('/api/products', productsRouter)
 app.use('/api/shoppingCarts', shoppingCartsRouter)
 
