ccee1a1368ed914e78e30209c3cf809cb065a992
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-10-08 21:25:52 -0500

    testing the fetching with proxy enabled

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,47 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes, json } from 'react-router-dom';
 
 import HomeDisplay from './components/HomeDisplay';
 import ProductInventory from './components/ProductInventory';
 import ShoppingCarts from './components/ShoppingCarts';
 
 export default function App() {
+  const [data, setData] = React.useState(null);
+
+  const getItems = () => {
+    fetch("/api/products").then((res) => res.json()).then((data) => setData(data))
+  }
+
+  React.useEffect(
+    getItems, [])
+
+
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/inventory">Inventory</Link>
-            </li>
-            <li>
-              <Link to="/cart">Cart</Link>
-            </li>
-          </ul>
-        </nav>
+    <>
+      <h1>TEST</h1><Router>
+        <h1>{JSON.stringify(data)}</h1>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/inventory">Inventory</Link>
+              </li>
+              <li>
+                <Link to="/cart">Cart</Link>
+              </li>
+            </ul>
+          </nav>
 
 
-        <Routes>
-          <Route path="/" element={<HomeDisplay />} />
-          <Route path="/inventory" element={<ProductInventory />} />
-          <Route path="/cart" element={<ShoppingCarts />} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/" element={<HomeDisplay />} />
+            <Route path="/inventory" element={<ProductInventory />} />
+            <Route path="/cart" element={<ShoppingCarts />} />
+          </Routes>
+        </div>
+      </Router></>
   );
 }
