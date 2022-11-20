085bac23253dc44d561b0f320eb16c3588858f76
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-11-20 16:28:12 -0600

    adding option for port in case it is not set up

--- server/src/main.js
+++ server/src/main.js
@@ -1,7 +1,7 @@
 import app from './server.js'
 
 const PORT = 8080;
-const server = app.listen(PORT, () => {
+const server = app.listen(process.env.PORT || PORT, () => {
     console.log(`Server currently listeingint ot port : ${server.address().port}`)
 })
 
