46b4c2830375490cb88c2a729675ede236299887
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-11-20 23:00:17 -0600

    Inventory Drop down

--- client/src/components/CartInventoryDropDown.jsx
+++ client/src/components/CartInventoryDropDown.jsx
@@ -1,33 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
 const CartInventoryDropDown = ({ cartData }) => {
+    const [activeCartID, setActiveCart] = useState();
+    const [activeProducts, setActiveProducts] = useState([]);
 
-
-    /**
-     * TODO update handle change to update context from the server side
-     */
-
-    const handleChange = () => {
-        return alert('hey this hould be updated ')
+    const handleCartChange = (event) => {
+        console.log(`event ${event.target.value}`) // THE VALUE BEING PASSED IS THE ID TO THEN FETCH
+        setActiveCart(event.target.value)
     }
-    return (
-        <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Age</InputLabel>
-            <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={'age'}
-                label="Age"
-                onChange={handleChange}
-            >
-                {
-                    cartData.map((row) =>
-                        (<MenuItem key={row.id} value={row.id}>{row.title}</MenuItem>)
-                    )
+
+    const handleProductChange = (event) => {
+        console.log(`event ${event}`)
+        setActiveProducts(event.target.value)
+    }
+    // will get updated everytime the active cart changes
+    useEffect(
+        () => {
+            async function fetchItemsCart() {
+                if (activeCartID) {
+                    let url = `/api/shppingCarts/${activeCartID}/products`
+                    let items = fetch(url, { method: "GET" }).then((res) => res.json())
+                    return items
                 }
-            </Select>
-        </FormControl>
+                return [];
+            }
+            let products = fetchItemsCart()
+            console.log('debugging useEffect')
+            console.log(activeCartID)
+            setActiveProducts(products)
+        }, [activeCartID])
+    return (
+        <>
+            <FormControl fullWidth>
+                <InputLabel id="shoppingCart-select-label">ShoppingCart</InputLabel>
+                <Select
+                    labelId="shoppingCart-dropdwon"
+                    id="shoppingCart-dropdown-select"
+                    value={''}
+                    label="Cart"
+                    onChange={handleCartChange}
+                >
+                    {cartData.map((row) => (<MenuItem key={row.id} value={row.id}>{row.title}</MenuItem>)
+                    )}
+                </Select>
+            </FormControl>
+            <FormControl fullWidth>
+                <InputLabel id="cartProducts-select-label">ShoppingCart</InputLabel>
+                <Select
+                    labelId="cartProducts-dropdwon"
+                    id="shoppingCart-dropdown-select"
+                    value={''}
+                    label="CartProducts"
+                    onChange={handleProductChange}
+                >
+
+                </Select>
+            </FormControl>
+        </>
     )
 }
 
