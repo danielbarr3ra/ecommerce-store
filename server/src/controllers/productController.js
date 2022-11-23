ba464b9a1cdcee00f70907ad093043c0d0d4a3a9
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-11-22 23:11:02 -0600

    adding serivce and controler template fro products

---
+++ server/src/controllers/productController.js
@@ -0,0 +1,29 @@
+import * as productService from '../services/productService.js'
+
+const getAllProducts = async (req, res) => {
+    res.send()
+}
+
+const getProduct = async (req, res) => {
+    res.send()
+}
+
+const createProduct = async (req, res) => {
+    res.send()
+}
+
+const updateProduct = async (req, res) => {
+    res.send()
+}
+
+const deleteProduct = async (req, res) => {
+    res.send()
+}
+
+export {
+    getAllProducts,
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
+}
---
+++ server/src/services/productService.js
@@ -0,0 +1,30 @@
+import { productsDao as ProductApi } from "../daos/index.js";
+
+const getAllProducts = async () => {
+
+}
+
+const getProduct = async () => {
+
+}
+
+const updateProduct = async () => {
+
+}
+
+const createProduct = async () => {
+
+}
+
+const deleteProduct = async () => {
+
+}
+
+export {
+    getAllProducts,
+    getProduct,
+    updateProduct,
+    createProduct,
+    deleteProduct
+}
+
