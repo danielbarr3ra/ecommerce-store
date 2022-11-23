4c78847a9e87839b1d1c10a12bf1ce4ab2f54195
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-11-22 22:45:00 -0600

    adding cart service

---
+++ server/src/services/cartService.js
@@ -0,0 +1,39 @@
+import { cartsDao as CartsApi } from "../daos/index.js";
+
+const getAllCarts = async () => {
+
+}
+
+const createCart = async () => {
+
+}
+
+const deleteCart = async () => {
+
+}
+
+const useCart = async () => {
+
+}
+
+const addProduct = async () => {
+
+}
+
+const removeProduct = async () => {
+
+}
+
+const assignCartToOrder = async () => {
+
+}
+
+export {
+    getAllCarts,
+    createCart,
+    deleteCart,
+    useCart,
+    addProduct,
+    removeProduct,
+    assignCartToOrder,
+}
