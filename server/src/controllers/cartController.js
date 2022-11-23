9ea2195726caf84ec8451ae49d0b63a17ff8ce9c
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-11-22 23:02:54 -0600

    adding cart controler

---
+++ server/src/controllers/cartController.js
@@ -0,0 +1,36 @@
+import * as cartService from '../services/cartService.js'
+
+// should match the functions in the routes.
+
+const getAllCarts = async (req, res) => {
+    res.send()
+}
+
+const getCart = async (req, res) => {
+    res.send()
+}
+
+const addCart = async (req, res) => {
+    res.send()
+}
+
+const deleteCart = async (req, res) => {
+    res.send()
+}
+
+const addProduct = async (req, res) => {
+    res.send()
+}
+
+const removeProduct = async (req, res) => {
+    res.send()
+}
+
+export {
+    getAllCarts,
+    getCart,
+    addCart,
+    deleteCart,
+    addProduct,
+    removeProduct
+}
